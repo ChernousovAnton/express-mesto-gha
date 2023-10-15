8720f6c9b47328afbc52bbef011a7ac6588da304
ChernousovAnton <Chantony_uz@mail.ru>
2023-10-15 22:56:13 +0300

    corrections after first review

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,10 +14,12 @@ const doesCardExist = (req, res, next) => {
     .then((data) => {
       if (!data) {
         res.status(404).send({ message: 'Запрашиваемая карточка не найдена' });
+        return;
       }
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
+      return;
     });
   next();
 };
--- controllers/users.js
+++ controllers/users.js
@@ -14,10 +14,12 @@ const doesUserExist = (req, res, next) => {
     .then((data) => {
       if (!data) {
         res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+        return;
       }
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
+      return;
     });
   next();
 };
